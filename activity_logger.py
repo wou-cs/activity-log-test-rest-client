31b270d569f986e52ed54fb89ef6495c6c1cb7e7
Chris Brooks <chris@chrisbrooks.org>
2018-10-17 11:50:56 -0700

    Making GETs more resilient to different plural return types and labels

--- activity_logger.py
+++ activity_logger.py
@@ -13,7 +13,24 @@ def _get_users(url):
         if r.status_code == 200:
             print(f"Get activities SUCCESS at {get_url}")
             activities = json.loads(r.text)
-            single_activity_id = activities["activities"][0]["id"]
+            single_activity_id = 0
+            if isinstance(activities, dict):
+                if "activities" in activities:
+                    single_activity_id = activities["activities"][0]["id"]
+                elif "activity_log" in activities:
+                    single_activity_id = activities["activity_log"][0]["id"]
+                elif "Activity_Log" in activities:
+                    single_activity_id = activities["Activity_Log"][0]["id"]
+                elif "activityLog" in activities:
+                    single_activity_id = activities["activityLog"][0]["id"]
+                elif "users" in activities:
+                    single_activity_id = activities["users"][0]["id"]
+                else:
+                    single_activity_id = activities["activitylog"][0]["id"]
+            elif isinstance(activities, list):
+                single_activity_id = activities[0]["id"]
+            else:
+                print("Error: JSON returned from Get neither list nor dict")
             r = requests.get(get_url + str(single_activity_id))
             if r.status_code == 200:
                 print(f"    Get single activity {single_activity_id} SUCCESS")
@@ -36,8 +53,9 @@ def _new_user(url):
     }
     try:
         r = requests.post(post_url, json=new_activity)
-        if r.status_code == 201:
+        if r.status_code == 201 or 200:
             print(f"Post new activity SUCCESS at {post_url}")
+            print(r.text)
             print(json.loads(r.text))
         else:
             print(f"Post new activity FAILURE: {r.text}")
